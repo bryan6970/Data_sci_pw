5b53facd9a8969e8b1e0975fcef2e14001f2ad70
bryan6970 <thacubingguy@gmail.com>
2023-03-15 21:34:40 +0800

    Fixed improper pd cleaning

--- Datasets/Clean_holidays.py
+++ Datasets/Clean_holidays.py
@@ -4,7 +4,7 @@
 import pandas as pd
 
 # Get limitations
-with open(r"Cleaned_datasets\limitations.txt", "r") as f:
+with open(r"limitations.txt", "r") as f:
     limitations = f.readlines()[-1].split(',')
     start_date, end_date = [pd.to_datetime(date) for date in limitations]
 
