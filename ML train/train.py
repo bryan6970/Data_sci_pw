858525a6022f1e013973bb9584649cb3ef123268
bryan6970 <thacubingguy@gmail.com>
2023-03-15 12:18:21 +0800

    Added ML code

--- ML train/train.py
+++ ML train/train.py
@@ -9,11 +9,42 @@ import numpy as np
 import pandas as pd
 
 # Load data
-data = pd.read_csv(r"C:\Users\wong2\PycharmProjects\Data_sci_pw\Datasets\final_dataset.csv")
+df = pd.read_csv(r"C:\Users\wong2\PycharmProjects\Data_sci_pw\Datasets\final_dataset.csv")
+
+# Parse dataset
+print(df)
+
+# Remove DateTime col
+df = df.shuffle(frac=1)
+df = df.drop("DateTime", axis=1)
+
+# Separate the Energy col
+energy_data = df[["Energy"]]
+
+normal_data = df.drop("Energy", axis=1)
+
+print(energy_data)
+
+print("\n\n\n\n")
+
+print(normal_data)
+
+# Get training and testing data
+# 513 rows allocated to testing
+
+# Randomly select the rows to remove
+test_energy_df = energy_data.sample(513)
+test_normal_df = normal_data.sample(513)
+
+# Remove the test rows from the original DataFrame
+train_energy_df = df.drop(test_energy_df.index)
+train_normal_df = df.drop(test_normal_df.index)
+
+print('Normal df', test_normal_df)
 
 # Define model
 model = tf.keras.Sequential([
-    tf.keras.layers.LSTM(64, activation='relu', return_sequences=True),
+    tf.keras.layers.LSTM(64, activation='relu', return_sequences=True, input_shape= (11,52000)),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.LSTM(32, activation='relu'),
     tf.keras.layers.Dropout(0.2),
@@ -24,10 +55,12 @@ model = tf.keras.Sequential([
 model.compile(optimizer='adam', loss='mse')
 
 # Train model
-model.fit(params, correct answer, epochs=100, batch_size=32)
+model.fit(train_normal_df, train_energy_df, epochs=100, batch_size=32)
 
 # Evaluate model
-loss = model.evaluate(params, correct answer)
+loss = model.evaluate(test_normal_df, test_energy_df)
 
 # Make predictions
-predictions = model.predict(params)
+predictions = model.predict(test_normal_df)
+
+model.save('Model')
