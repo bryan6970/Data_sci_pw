99d6fc5491eb874d3e26782b60a57661ef53a3b3
Bryan <thacubingguy@gmail.com>
2023-07-03 10:27:23 +0800

    clean dataset

---
+++ Datasets/cyl/main.py
@@ -0,0 +1,34 @@
+import pyforest
+import pandas as pd
+import os
+from repository_utils import get_repo_root
+
+df = pd.read_csv("stats.csv")
+
+# File not completed yet
+ElectricityDemand_df = pd.read_csv(os.path.join(get_repo_root(), "Datasets/Yu feng"))
+
+ElectricityDemand_df.resample("Y").mean()
+
+df.columns = df.iloc[0]
+df = df[1:]
+
+df.drop("Unnamed: 0", axis=1, inplace=True)
+
+df.rename(columns={"Date Series": "DateTime"})
+
+df.dropna()
+
+df["DateTime"] = pd.to_datetime(df["DateTime"], format="mixed")
+
+df.sort_values(by="Date", inplace=True)
+
+correlation = df.corrwith(ElectricityDemand_df["DEMAND (MW)"])
+
+for col, corr in correlation.items():
+    if -0.5 <= corr <= 0.5:
+        df.drop(col, axis=1)
+
+print(df)
+
+df.to_csv("stats.csv")
