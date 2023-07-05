8063337ccb029ec246082bbf883ef50a85a21c23
Bryan <thacubingguy@gmail.com>
2023-07-05 18:53:14 +0800

    edited plot_df() function

--- repository_utils.py
+++ repository_utils.py
@@ -26,7 +26,8 @@ def import_file_from_repository(relative_path):
     # Return the imported module
     return module
 
-def plot_df(df, figsize=(8, 6), plot_type="line", together=False):
+
+def plot_df(df, plot_type="line", round_=2, save=False, together=False, figsize=(8, 6)):
     """
     Plot DataFrame columns against the index.
 
@@ -35,43 +36,81 @@ def plot_df(df, figsize=(8, 6), plot_type="line", together=False):
         figsize (tuple, optional): Figure size in inches (width, height). Default is (8, 6).
         plot_type (str, optional): Type of plot. Can be "line", "bar", "scatter", or "area". Default is "line".
         together (bool, optional): If True, plots all columns on the same plot. If False, each column has a separate plot. Default is False.
+        round_ (int or None, optional): Number of decimal places to round the values. Default is 2.
+        save (bool, optional): If True, saves the plots as images. Default is False.
 
     Returns:
         None
     """
+
+    def plot_according_to_type(plot_type_):
+        if plot_type == "line":
+            ax.plot(df.index, df[column].astype(str), label=column)
+            return True
+        elif plot_type == "bar":
+            ax.bar(df.index, df[column].astype(str), label=column)
+            return True
+        elif plot_type == "barh":
+            ax.barh(df.index, df[column].astype(str), label=column)
+            return True
+        elif plot_type == "hist":
+            ax.hist(df[column].astype(str), label=column)
+            return True
+        elif plot_type == "box":
+            ax.boxplot(df[column].astype(str), label=column)
+            return True
+        elif plot_type == "kde" or plot_type == "density":
+            ax.plot(df.index, df[column].astype(str), label=column)
+            ax.fill_between(df.index, df[column].astype(str), alpha=0.5, label=column)
+            return True
+        elif plot_type == "area":
+            ax.fill_between(df.index, df[column].astype(str), alpha=0.5, label=column)
+            return True
+        elif plot_type == "pie":
+            ax.pie(df[column].astype(str), labels=df.index, autopct="%1.1f%%")
+            return False
+        else:
+            raise ValueError(f"{plot_type} is not available")
+
+    set_label = False
+
+    if round_:
+        df = df.round(round_)
+
     if together:
         fig, ax = plt.subplots(figsize=figsize)
         for column in df.columns:
-            if plot_type == "line":
-                ax.plot(df.index, df[column].astype(str), label=column)
-            elif plot_type == "bar":
-                ax.bar(df.index, df[column].astype(str), label=column)
-            elif plot_type == "scatter":
-                ax.scatter(df.index, df[column].astype(str), label=column)
-            elif plot_type == "area":
-                ax.fill_between(df.index, df[column].astype(str), alpha=0.5, label=column)
+            set_label = plot_according_to_type(plot_type)
 
-        ax.set_xlabel('Index')
-        ax.set_ylabel('Value')
-        ax.legend()
+        if set_label:
+            ax.set_xlabel(df.index)
+            ax.set_ylabel("Columns")
+            ax.legend()
+
+        plt.title(f"Graph of {df.index.name} against Columns")
+        plt.legend()
+
+        if save:
+            plt.savefig(f"Graph of {df.index.name} against Columns {df.columns}")
+
         plt.show()
     else:
         for column in df.columns:
             fig, ax = plt.subplots(figsize=figsize)
-            if plot_type == "line":
-                ax.plot(df.index, df[column].astype(str))
-            elif plot_type == "bar":
-                ax.bar(df.index, df[column].astype(str))
-            elif plot_type == "scatter":
-                ax.scatter(df.index, df[column].astype(str))
-            elif plot_type == "area":
-                ax.fill_between(df.index, df[column].astype(str), alpha=0.5)
+            plot_according_to_type(plot_type)
 
-            ax.set_xlabel('Index')
-            ax.set_ylabel('Value')
-            ax.set_title(column)
+            ax.set_xlabel(df.index.name)
+            ax.set_ylabel(column)
+            ax.set_title(f"Graph of {df.index.name} against Columns {column}")
+
+            plt.legend()
+
+            if save:
+                plt.savefig(f"Graph of {df.index.name} against Columns {column}")
+
             plt.show()
     fig, ax = plt.subplots(figsize=figsize)
+
 
 def get_repo_root():
     return os.path.dirname(os.path.abspath(__file__))
